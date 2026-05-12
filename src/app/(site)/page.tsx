c5157eced826bdb2d9104ffa3f9cab00eb63cf1c
omerf130 <ofaclieru@gmail.com>
2026-05-12 09:47:35 +0300

    categories added

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from "react";
 import { Spinner } from "@/components/Spinner";
 import { Hero } from "@/features/home/Hero";
-import { FeaturedProducts } from "@/features/home/FeaturedProducts";
 import { CategoryTiles } from "@/features/home/CategoryTiles";
 import { BestSellers } from "@/features/home/BestSellers";
 import { EditorialSection } from "@/features/home/EditorialSection";
@@ -13,9 +12,6 @@ export default function HomePage() {
   return (
     <>
       <Hero />
-      <Suspense fallback={<Spinner />}>
-        <FeaturedProducts />
-      </Suspense>
       <Suspense fallback={<Spinner />}>
         <CategoryTiles />
       </Suspense>
